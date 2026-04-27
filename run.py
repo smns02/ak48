29e82b6611f9dd748fdd6b209be0775c7e976fd8
smns02 <linnaung94sgu@gmail.com>
2026-04-27 15:15:55 +0630

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 import main
-
-if __name__ == "__main__":
+# main.main_run() အစား အောက်ကလို စမ်းကြည့်ပါ
+if hasattr(main, 'main_run'):
     main.main_run()
+elif hasattr(main, 'login'):
+    main.login()
+elif hasattr(main, 'menu'):
+    main.menu()
+else:
+    print("Function name not found in main.so")
